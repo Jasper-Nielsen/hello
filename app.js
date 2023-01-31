c849a736966282d8a19731f87ad2c0351622734f
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-01-31 13:22:53 +0100

    with name and age

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 function sayHello() {
   document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("input").value + "!";
+    "Hej " + document.querySelector("#name").value;
+  document.querySelector("h4").textContent =
+    "Din alder er: " + document.querySelector("#age").value;
 }
 
 document.querySelector("button").addEventListener("click", sayHello);
